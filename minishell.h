921dc8ec2af4c99c51cf285d49915245aad16e3b
Elias Salmela <esalmela@c1r6p16.hive.fi>
2024-04-11 15:18:27 +0300

    path to header

--- minishell.h
+++ minishell.h
@@ -6,12 +6,14 @@
 /*   By: esalmela <esalmela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:56:05 by esalmela          #+#    #+#             */
-/*   Updated: 2024/04/11 13:56:51 by esalmela         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:17:36 by esalmela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "Libft/libft.h"
 
 typedef struct s_split
 {
