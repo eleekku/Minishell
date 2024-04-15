8dbe495f3991607bcba5e72d4f8a6e731f19e199
Deivylex <deivylex93@gmail.com>
2024-04-15 18:21:56 +0300

    shelllvl

--- minishell.c
+++ minishell.c
@@ -1,13 +1,66 @@
 # include "minishelld.h"
 
-int input_check(t_data *content)
-{
+//think about "heredoc"
 
-}
+/*int input_check(t_data *content)
+{
+  //lexer stuff
+}*/
 void exit_error(char *str)
 {
   printf("%s\n", str);
   exit(1);
+}
+void  update_envp(t_data  *content)
+{
+  char **envp;
+  char *lvl;
+  int num;
+  int i;
+
+  envp = content->env;
+  i = 0;
+  while (envp[i]  && ft_strncmp(envp[i], "SHLVL=", 6))
+    i++;
+  num = ft_atoi(envp[i] + 6);
+  lvl = ft_strjoin("SHLVL=", ft_itoa(num + 1));
+  if (!lvl)
+  {
+    //free envp;
+    exit_error("Error Malloc SHLVL");
+  }
+  i = -1;
+  while (envp[++i])
+  {
+    if (ft_strncmp(envp[i], "SHLVL=", 6) == 0)
+    {
+      free (envp[i]);
+      envp[i] = lvl;
+      free (lvl);
+    }
+    printf("%s\n", envp[i]);
+  }
+  content->env = envp;
+}
+void  create_envp(char **env, t_data *content)
+{
+  char **envp;
+  int i;
+  int n;
+
+  n = 0;
+  while (env[n])
+    n++;
+  envp = (char **)malloc(sizeof(char *) * n);
+  i = 0;
+  while (i < n)
+  {
+    envp[i] = ft_strdup(env[i]);
+    if (envp[i] == NULL)
+        perror("Error al asignar memoria");
+    i++;
+  }
+  content->env = envp;
 }
 
 int main(int ac, char **av, char **envp) 
@@ -18,15 +71,16 @@ int main(int ac, char **av, char **envp)
   if (ac == 0 || ac > 1)
       exit_error("Invalid input\n");
   printf("Welcome to Minishell los pran...\n");
-  content.env = envp;
-  while ((input = readline("minishell$ ")) != NULL) 
+  create_envp(envp, &content);
+  update_envp(&content);
+  /*while ((input = readline("minishell$ ")) != NULL) 
   {
     if (strlen(input) > 0) 
       add_history(input);
     content.str_rl = &input;
-    input_check(&content);
+    input_check(&content);//lexer for the parse
     printf("%s\n", input);
     free(input);
-  }
+  }*/
   return (0);
 }
