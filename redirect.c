c453f1366624d1d0b95cbe1b4a0c6affb33c5413
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2024-05-28 19:59:18 +0300

    redirect

---
+++ redirect.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esalmela <esalmela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 19:37:43 by esalmela          #+#    #+#             */
+/*   Updated: 2024/05/28 19:37:44 by esalmela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "minishell.h"
+
+int	open_doc(char *file, int i)
+{
+	int	fd;
+
+	if (i == 0)
+	{
+		fd = open(file, O_RDONLY);
+		if (fd == -1)
+		{
+			printf("minishell$ %s: ", file);
+			perror("");
+		}
+	}
+	else
+	{
+		fd = open(file, O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (fd == -1)
+		{
+			printf("minishell$ %s: ", file);
+			perror("");
+		}
+	}
+	return (fd);
+}
+
+void	redirect(t_data	*cnt, int i)
+{
+	int fdin;
+	int j;
+
+	j = -1;
+	while (cnt->parse[i].rec_file[++j])
+		if (cnt->parse[i].rec_file[j][0] == '>' && cnt->parse[i].rec_file[j][1] != '>')
+			fdin = open_doc(((ft_strchr(cnt->parse[i].rec_file[j], '>') + 1)), 0);
+}
