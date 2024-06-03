e4eb28c5095ae10a7285a764a67a8f71aa16b693
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2024-06-03 17:46:20 +0300

    here_doc should be ok

--- redirect.c
+++ redirect.c
@@ -12,23 +12,32 @@
 
 #include "minishell.h"
 
-void	here_doc(char *limiter)
+void	here_doc(char *limiter, t_data *cnt)
 {
 	int		pipefd[2];
 	char	*line;
-	printf("im here\n");
-	pipe(pipefd);
-	while (1)//ft_strncmp(line, limiter, ft_strlen(limiter)) != 0
+
+	if (cnt->here_doc_fd > 0)
+	{
+		dup2(cnt->stdin_backup, STDIN);
+		close(cnt->stdin_backup);
+		cnt->stdin_backup = dup(STDIN);
+	}
+	if (pipe(pipefd) == -1)
+		exit (1);
+	while (1)
 	{
 		line = readline(" > ");
 		if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
-			break;
-		ft_putstr_fd(line, pipefd[1]);
+			break; 
+		ft_putendl_fd(line, pipefd[1]);
 		free(line);
 	}
-	free(line);
 	close(pipefd[1]);
-	//return (pipefd[0]);
+	free(line);
+	cnt->here_doc_fd = pipefd[0];
+	dup2(cnt->here_doc_fd, STDIN);
+	close(cnt->here_doc_fd);
 }
 
 void	open_in_doc(char *file)
@@ -91,7 +100,8 @@ int	redirect(t_data	*cnt, int i)
 		if (cnt->parse[i].rec_file[j][0] == '<' && cnt->parse[i].rec_file[j][1] != '<')
 			open_in_doc((ft_strchr(cnt->parse[i].rec_file[j], '<') + 1));
 		if (cnt->parse[i].rec_file[j][0] == '<' && cnt->parse[i].rec_file[j][1] == '<')
-			here_doc(ft_strchr(cnt->parse[i].rec_file[j], '<') + 2);	
+			here_doc(ft_strchr(cnt->parse[i].rec_file[j], '<') + 2, cnt);	
 	}
+	ft_printf(2, "exited redirect loop\n");
 	return (0);
 }
